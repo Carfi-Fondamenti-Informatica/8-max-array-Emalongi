6581f64adfd4f21bbf8f57acfab05bc912753a9d
Emalongi <91636607+Emalongi@users.noreply.github.com>
2021-12-09 12:41:09 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
+int main() {
+    int a=0;
+    float max=0;
+    cin>>a;
+    float lol[a];
+    for(int i=0; i<a; i++){
+        float b=0;
+        cin>>b;
+       lol[i]=b;
+    }
+    cout<< mass( a, lol ,max );
 
-int main(){
-  return 0;
+
+    return 0;
 }
