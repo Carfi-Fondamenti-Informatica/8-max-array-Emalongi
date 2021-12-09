ec31879ad2a9647b75c3528c3a1594b92668ed29
Emalongi <91636607+Emalongi@users.noreply.github.com>
2021-12-09 12:42:29 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,15 @@
 
+#include <iostream>
+#include "lib.h"
+using namespace std;
+ // N numero di elementi, i contatore for , max per valore massimo,
+ float mass(int N, float array[], float max) {
+
+     max = array[0];
+     for (int i = 1; i < N; i++) {
+         if (array[i] > max) {
+             max=array[i];
+
+         }
+     } return max;
+ }
